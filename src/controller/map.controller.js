e7e495660f9a9b15a9fbddc6440010c12737ff38
OMP-49 <om@Oms-MacBook-Air.local>
2022-12-07 23:05:24 -0500

    Added controller to validate requests and call service functions

---
+++ src/controller/map.controller.js
@@ -0,0 +1,72 @@
+// import { APIRespose } from "../models/api-response.model";
+import pathService from "../services/impl/path.service.js";
+import { WALK_settings } from "../utils/constants.js";
+import Logger from "../utils/logger.js";
+pathService.init(WALK_settings);
+
+const LOGGER = Logger("index.js");
+class MapController {
+    async getMinElevationPath(req, res, next) {
+        let startCoordiantes = req.body["start"]?.coordinates;
+        let endCordinates = req.body["end"]?.coordinates;
+        let deviation = req.body["percentage"];
+
+        if (startCoordiantes != undefined && endCordinates != undefined) {
+            try {
+                let source = {
+                    lat: startCoordiantes[0],
+                    lng: startCoordiantes[1],
+                };
+                let dest = {
+                    lat: endCordinates[0],
+                    lng: endCordinates[1],
+                };
+                let result = await pathService.calculateRequestPath(source, dest, deviation, false);
+                return res.status(200).json(result);
+            } catch (error) {
+                if (typeof error === "string") {
+                    return res.status(400).json({ status: 400, message: error.toUpperCase() });
+                } else if (error instanceof Error) {
+                    return res.status(400).json({ status: 400, message: error.message });
+                } else {
+                    return res.status(400).json({ status: 400, message: "INTERNAL_SERVER_ERROR" });
+                }
+            }
+        } else {
+            return res.status(400).json({ status: 400, message: "INVALID_REQUEST" });
+        }
+    }
+
+    async getMaxElevationPath(req, res, next) {
+        let startCoordiantes = req.body["start"]?.coordinates;
+        let endCordinates = req.body["end"]?.coordinates;
+        let deviation = req.body["percentage"];
+
+        if (startCoordiantes != undefined && endCordinates != undefined) {
+            try {
+                let source = {
+                    lat: startCoordiantes[0],
+                    lng: startCoordiantes[1],
+                };
+                let dest = {
+                    lat: endCordinates[0],
+                    lng: endCordinates[1],
+                };
+                let result = pathService.calculateRequestPath(source, dest, deviation, true);
+                return res.status(200).json(result);
+            } catch (error) {
+                if (typeof error === "string") {
+                    return res.status(400).json({ status: 400, message: error.toUpperCase() });
+                } else if (error instanceof Error) {
+                    return res.status(400).json({ status: 400, message: error.message });
+                } else {
+                    return res.status(400).json({ status: 400, message: "INTERNAL_SERVER_ERROR" });
+                }
+            }
+        } else {
+            return res.status(400).json({ status: 400, message: "INVALID_REQUEST" });
+        }
+    }
+}
+
+export default new MapController();
