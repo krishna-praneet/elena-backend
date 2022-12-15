87e0cd1d794055c39d4afe2301559be191393020
Krishna Praneet Gudipaty <praneet1998@gmail.com>
2022-12-15 03:14:42 -0500

    Setup Jest testing backedn

---
+++ test/index.test.js
@@ -0,0 +1,14 @@
+import mapModel from  "../src/model/map.model";
+// import mapController from "../src/controller/map.controller";
+
+// test("Testing generateGraph method of Map Model", () => {
+//     expect().toBe();
+// })
+
+test("Testing generateGraph method of Map Model", () => {
+    // expect(mapModel.generateGraph()).toBe();
+})
+
+test("Testing getElevations method of Map Model", () => {
+    // expect(mapModel.getElevations).toBe();
+})
