821c63f9b0a04d7f465b9d19d19033557f2c1610
OMP-49 <om@Oms-MacBook-Air.local>
2022-12-16 22:35:38 -0500

    Removed redundant code

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,3 @@
-import MapModel from "../src/model/map.model.js";
-// const mapMode = require("../src/model/map.model").MapModel;
-import { expect } from "chai";
-import mapController from "../src/controller/map.controller";
-
 before(async (done) => {
-    console.log("Inhere");
     await pathService.init(MAP_WALK_SETTINGS);
 });
